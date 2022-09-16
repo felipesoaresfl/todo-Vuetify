dcd5672cd40d4d64cb3359ad14fec7bc8aae4c71
Felipe Soares <felipe.soares013@outlook.com>
2022-09-16 12:00:34 -0300

    feito o login do usuario com validação

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,11 +1,23 @@
 <template>
   <v-container class="white" id="login-page">
+    <v-snackbar
+      v-model="snackbar.show"
+      :value="true"
+      color="green darken-1"
+      absolute
+      left
+      shaped
+      top
+    >
+      <h3 style="font-size: 20 px">
+        {{ snackbar.message }}
+      </h3>
+    </v-snackbar>
     <v-row class="d-flex justify-center">
       <v-col cols="6">
         <v-tabs
           fixed-tabs
-          background-color="green darken-1
-"
+          background-color="green darken-1"
           color="white"
           v-model="tabsForm"
         >
@@ -21,7 +33,7 @@
             <v-form ref="form" v-model="valid" lazy-validation>
               <v-text-field
                 v-model="name"
-                :counter="20"
+                :counter="30"
                 :rules="nameRules"
                 label="Nome"
                 required
@@ -31,6 +43,14 @@
                 :rules="emailRules"
                 label="E-mail"
                 required
+              ></v-text-field>
+              <v-text-field
+                v-model="username"
+                label="Usuário"
+                :counter="20"
+                required
+                :rules="usernameRules"
+                v-on:keyup.enter="login"
               ></v-text-field>
               <v-text-field
                 v-model="password"
@@ -58,6 +78,7 @@
                 :disabled="!valid"
                 color="success"
                 class="mr-4"
+                :loading="loading"
                 @click="validate"
                 :to="{ name: 'taskList' }"
               >
@@ -68,10 +89,11 @@
           <v-tab-item key="register">
             <v-form class="text-center white">
               <v-text-field
-                v-model="email"
-                label="Email"
-                :rules="emailRules"
+                v-model="username"
+                label="Usuário"
                 required
+                :rules="usernameRules"
+                v-on:keyup.enter="login"
               ></v-text-field>
               <v-text-field
                 v-model="password"
@@ -79,11 +101,14 @@
                 type="password"
                 :rules="passwordRules"
                 required
+                v-on:keyup.enter="login"
               ></v-text-field>
               <v-btn
+                :loading="loading"
+                :disabled="!valid"
                 color="success"
                 class="justify-space-between"
-                :to="{ name: 'taskList' }"
+                @click="login"
               >
                 Entrar
               </v-btn>
@@ -94,7 +119,10 @@
     </v-row>
   </v-container>
 </template>
+
 <script>
+import AuthApi from "@/api/auth.api.js";
+
 export default {
   data: () => ({
     successPass: false,
@@ -102,17 +130,27 @@ export default {
     tabsForm: null,
     valid: true,
     name: "",
+    username: "",
+    email: "",
+    confirmPassword: "",
+    password: "",
+    loading: false,
+    snackbar: {
+      show: false,
+      message: "",
+    },
     nameRules: [
       (v) => !!v || "Nome é obrigatório",
-      (v) => (v && v.length <= 10) || "Nome deve conter menos de 10 caracteres",
+      (v) => (v && v.length <= 30) || "Nome deve conter menos de 30 caracteres",
     ],
-    email: "",
+    usernameRules: [
+      (v) => !!v || "Usuário é obrigatório",
+      (v) => (v && v.length <= 20) || "Nome deve conter menos de 20 caracteres",
+    ],
     emailRules: [
       (v) => !!v || "O e-mail é obrigatório",
       (v) => /.+@.+\..+/.test(v) || "E-mail inválido",
     ],
-    confirmPassword: "",
-    password: "",
     passwordRules: [
       (v) => !!v || "A senha é obrigatória.",
       (v) => (v && v.length > 5) || "A senha deve conter mais de 5 caracteres",
@@ -123,9 +161,30 @@ export default {
     validate() {
       this.$refs.form.validate();
     },
+    //logar usuário
     login() {
       this.loading = true;
+      AuthApi.login(this.username, this.password)
+        .then((user) => {
+          console.log("login ok", user);
+          this.saveLoggedUser(user);
+          this.$router.push({ name: "taskList" });
+        })
+        .catch((error) => {
+          console.log("login falhou", error);
+          this.snackbar.message = "Usuário ou senha inválidos!";
+          this.snackbar.show = true;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
     },
+    //salvar usuário logado no localstorage
+    saveLoggedUser(user) {
+      window.localStorage.setItem("loggedUser", user.id);
+      window.localStorage.setItem("loggedUserToken", user.token);
+    },
+    //validar campos de login e cadastro
     matchingPasswords() {
       if (this.password === this.confirmPassword) {
         return true;
